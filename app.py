dc25a1d5e23c82871d267306417f2925dcabbf36
Ben Asselmeier <benasselmeier@bens-mbp.lan>
2024-07-29 14:57:42 -0500

    split into sections

--- app.py
+++ app.py
@@ -2,44 +2,52 @@ from flask import Flask, request, redirect, url_for, render_template
 import os
 
 app = Flask(__name__)
-directory = 'text-files/'
+base_directory = 'text-files/'
+
+def read_files_from_directory(directory):
+    print(f"Reading files from directory: {directory}")  # Debug statement
+    files = sorted(os.listdir(directory))
+    file_contents = {}
+    for file in files:
+        try:
+            with open(os.path.join(directory, file), 'r') as f:
+                file_contents[file] = f.read()
+        except UnicodeDecodeError:
+            print(f"Skipping file {file} because it could not be decoded")
+    print(f"Files read from {directory}: {file_contents.keys()}")  # Debug statement
+    return file_contents
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    directory = 'text-files'
-    files = sorted(os.listdir(directory))
-    file_names = {format_filename(file): file for file in files}
-    file_contents = {}
-    for formatted_name, original_name in file_names.items():
-        try:
-            with open(os.path.join(directory, original_name), 'r') as f:
-                file_contents[formatted_name] = f.read()
-        except UnicodeDecodeError:
-            print(f"Skipping file {original_name} because it could not be decoded")
+    info_files = read_files_from_directory(os.path.join(base_directory, 'info'))
+    players_files = read_files_from_directory(os.path.join(base_directory, 'players'))
+    casters_files = read_files_from_directory(os.path.join(base_directory, 'casters'))
+
     if request.method == 'POST':
-        for formatted_name, original_name in file_names.items():
-            content = request.form.get(formatted_name)
-            if content is not None:
-                with open(os.path.join(directory, original_name), 'w') as f:
-                    f.write(content)
+        for directory, files in [('info', info_files), ('players', players_files), ('casters', casters_files)]:
+            for file in files:
+                content = request.form.get(file)
+                if content is not None:
+                    with open(os.path.join(base_directory, directory, file), 'w') as f:
+                        f.write(content)
         return redirect(url_for('home'))
 
     # Read the contents of the Fighters.txt file
     with open(os.path.join('resources', 'Fighters.txt'), 'r') as file:
         fighters = file.read().splitlines()
 
-    return render_template('home.html', files=file_contents, fighters=fighters)
+    return render_template('home.html', info_files=info_files, players_files=players_files, casters_files=casters_files, fighters=fighters)
 
 @app.route('/view/<filename>')
 def view(filename):
-    with open(os.path.join(directory, filename), 'r') as f:
+    with open(os.path.join(base_directory, filename), 'r') as f:
         content = f.read()
     return '<form action="/update/{}" method="POST"><textarea name="content" rows="30" cols="100">{}</textarea><br><input type="submit" value="Update"></form>'.format(filename, content)
 
 @app.route('/update/<filename>', methods=['POST'])
 def update(filename):
     content = request.form['content']
-    with open(os.path.join(directory, filename), 'w') as f:
+    with open(os.path.join(base_directory, filename), 'w') as f:
         f.write(content)
     return redirect(url_for('view', filename=filename))
 
@@ -47,10 +55,7 @@ def format_filename(filename):
     # Remove the file extension
     filename = os.path.splitext(filename)[0]
     # Replace dashes with spaces
-    filename = filename.replace('-', ' ')
-    # Capitalize each word
-    filename = filename.title()
-    return filename
+    return filename.replace('-', ' ')
 
 if __name__ == '__main__':
     app.run(debug=True)
