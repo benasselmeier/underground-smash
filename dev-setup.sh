c9e62649dd92e1c20532cb467754c1ba380b57c9
Ben Asselmeier <benasselmeier@bens-mbp.lan>
2024-07-29 13:12:54 -0500

    update gitignore again

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Navigate to your project directory
-cd web-ui/
-
 # Activate the virtual environment
 source venv/bin/activate
-
-# Go back up a directory
-cd ..
 
 # Run that shyt
 flask run
